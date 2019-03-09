3020fc516afd7cf0afa733b45ac2e96b29a4f56c
Alexander Donchuk <alex.dd84@gmail.com>
2019-03-09 12:13:23 +0300

    Update se_module.py

--- models/se_module.py
+++ models/se_module.py
@@ -1,17 +1,16 @@
-from torch import nn
+from torch.nn import AdaptiveAvgPool2d, Sequential, Linear, ReLU, Sigmoid, Module
 
 
-class SCSEBlock(nn.Module):
+class SCSEBlock(Module):
     def __init__(self, channel, red=16):
         super(SCSEBlock, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-                nn.Linear(channel, red), nn.ReLU(inplace=True),
-                nn.Linear(red, channel), nn.Sigmoid()
+        self.avg_pool = AdaptiveAvgPool2d(1)
+        self.fc = Sequential(
+            Linear(channel, red), ReLU(inplace=True),
+            Linear(red, channel), Sigmoid()
         )
 
     def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
+        a, b, _, _ = x.size()
+        y = self.fc(self.avg_pool(x).view(a, b)).view(a, b, 1, 1)
         return x * y
