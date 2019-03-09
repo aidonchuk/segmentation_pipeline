f3ab37085c413d3ffd98bacf2d4df96e2adf2ccd
Alexander Donchuk <alex.dd84@gmail.com>
2019-03-09 12:10:42 +0300

    Update se_module.py

--- models/se_module.py
+++ models/se_module.py
@@ -1,15 +1,13 @@
 from torch import nn
 
 
-class SELayer(nn.Module):
+class SCSEBlock(nn.Module):
     def __init__(self, channel, red=16):
-        super(SELayer, self).__init__()
+        super(SCSEBlock, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
-                nn.Linear(channel, red),
-                nn.ReLU(inplace=True),
-                nn.Linear(red, channel),
-                nn.Sigmoid()
+                nn.Linear(channel, red), nn.ReLU(inplace=True),
+                nn.Linear(red, channel), nn.Sigmoid()
         )
 
     def forward(self, x):
